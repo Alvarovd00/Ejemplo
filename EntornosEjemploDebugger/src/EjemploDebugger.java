a074fd7016cb171584772d1382ffd5d5674328ca
User <User@192.168.1.141>
2024-01-25 19:03:57 +0100

    Ejemplo

---
+++ EntornosEjemploDebugger/src/EjemploDebugger.java
@@ -0,0 +1,30 @@
+
+public class EjemploDebugger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		//EJEMPLO
+		
+		
+		
+		
+		for (int i=0;i<10;i++) {
+			System.out.println("iteración nº "+ i);
+		}
+		
+		ArrayList<Cliente> clientes = null;
+			
+		
+		
+		for(Cliente cliente:clientes) {	
+		
+			cliente.darDeAlta();
+			cliente.enviarSpam();
+			
+		}
+	}
+
+}
